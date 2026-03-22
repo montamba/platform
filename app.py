b38e587c748ccc3ad8cb0a5d1d627b2dd8f6a0a7
montamba <monmon272005@gmail.com>
2026-03-22 16:18:13 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return redirect(url_for("cyboard"))
+
+@app.route("/catering")
+def catering():
+    return render_template("catering.html")
+
+@app.route("/cyboard")
+def cyboard():
+    return render_template("cyboard.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/patient")
+def patient():
+    return render_template("patient.html")
+
